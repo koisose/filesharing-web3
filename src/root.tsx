fb7990eec115ce9e5154809816daac60cbf77947
koisose <plokotokbelajar@gmail.com>
2023-06-17 10:53:34 +0700

    Add Vercel Edge adapter and configurations

--- src/root.tsx
+++ src/root.tsx
@@ -3,20 +3,17 @@ import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.
 import { RouterHead } from './components/router-head/router-head';
 
 import './global.css';
-
+import "nes.css/css/nes.min.css";
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Dont remove the `<head>` and `<body>` elements.
-   */
+
 
   return (
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
+        <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet"/>
+          <link href="https://unpkg.com/nes.css/css/nes.css" rel="stylesheet" />
         <RouterHead />
       </head>
       <body lang="en">
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,112 +1,69 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
-import Counter from '~/components/starter/counter/counter';
-import Hero from '~/components/starter/hero/hero';
-import Infobox from '~/components/starter/infobox/infobox';
-import Starter from '~/components/starter/next-steps/next-steps';
+
 
 export default component$(() => {
   return (
     <>
-      <Hero />
-      <Starter />
-
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
-      </div>
-
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
+   <header class="bg-blue-500 text-white px-6 py-4">
+  <div class="flex items-center justify-between">
+    <div class="text-2xl font-semibold">
+     Filesharing Web3
+    </div>
+    {/*<nav class="space-x-4">*/}
+    {/*  <a href="#" class="hover:bg-blue-700 px-3 py-1 rounded">Home</a>*/}
+    {/*  <a href="#" class="hover:bg-blue-700 px-3 py-1 rounded">About</a>*/}
+    {/*  <a href="#" class="hover:bg-blue-700 px-3 py-1 rounded">Contact</a>*/}
+    {/*</nav>*/}
+  </div>
+   </header>
+    <div class="min-h-screen flex flex-col items-center justify-start bg-gray-100 py-4 sm:py-8">
+      <div class="w-full sm:w-1/2 mx-4 sm:mx-8">
+        <div class="bg-blue-500 p-8 mb-4 sm:mb-2  mt-4 border border-gray-300 shadow-md rounded">
+          <div class="flex items-center justify-center ">
+            <img class="my-5"  width="300" height="300" alt="Gravatar image example" src="https://www.gravatar.com/avatar" />
           </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
 
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{' '}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
 
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
+          <div class="flex items-center justify-center ">
+              <label class="nes-btn">
+                <span>Select your file</span>
+                <input type="file" />
+              </label>
+          </div>
+          <label for="default_select">Default select</label>
+          <div class="nes-select">
+          <select required id="default_select">
+            <option value="" disabled selected hidden>Select...</option>
+            <option value="0">To be</option>
+            <option value="1">Not to be</option>
+          </select>
+          </div>
+
+          <label for="default_select">Default select</label>
+          <div class="nes-select">
+          <select required id="default_select">
+            <option value="" disabled selected hidden>Select...</option>
+            <option value="0">To be</option>
+            <option value="1">Not to be</option>
+          </select>
+          </div>
         </div>
       </div>
+    </div>
+
+
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'Filesharing Web3',
   meta: [
     {
       name: 'description',
-      content: 'Qwik site description',
+      content: 'Share your file everywhere ',
     },
   ],
 };
